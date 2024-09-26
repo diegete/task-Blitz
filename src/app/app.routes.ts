67c5a87e7a3397303ec0acf0e5cc1a99f9b3df6b
diegete <di.estay@duocuc.cl>
2024-09-26 18:08:28 -0300

    inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { UserViewComponent } from './components/user-view/user-view.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent},
     {path: 'home', component:HomeComponent},
-    {path: 'login', component:LoginComponent},
-    {path: 'user-view', component:UserViewComponent},
+    {path: 'login', component:LoginComponent}
     
 ];
