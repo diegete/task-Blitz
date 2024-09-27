839e74fc737ce5fe78401dc99442b5da8648678b
diegete <di.estay@duocuc.cl>
2024-09-27 16:49:07 -0300

    "Updated home component with new CSS styles, added form for creating tasks, and implemented task creation functionality"

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,30 +1,62 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
 import { UserdataService } from '../../services/userdata.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CreateTasksService } from '../../services/create-tasks.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   userData: any;
-
-  constructor(private userService: UserdataService) {}
+  taskForm: FormGroup;
+  constructor(
+    private userService: UserdataService,
+    private taskService: CreateTasksService,
+    private formBuilder: FormBuilder
+  ) {
+    // Inicializar el formulario de creación de tareas
+    this.taskForm = this.formBuilder.group({
+      titulo: ['', [Validators.required]],
+      descripcion: ['', [Validators.required]],
+      carga: ['', [Validators.required, Validators.min(1), Validators.max(10)]],
+      proyecto: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
     if (this.userService.isLoggedIn()) {
       this.userService.getUserData().subscribe(data => {
         this.userData = data;
         console.log('Datos del usuario:', this.userData);
+        if (this.userData && this.userData.proyectos) {
+          console.log('Proyectos del usuario:', this.userData.proyectos);
+          
+        } else {
+          console.log('No se encontraron proyectos para el usuario');
+        }
       });
     } else {
       console.log('No hay un usuario autenticado');
     }
   }
- 
+
+  // Método para crear una tarea
+  createTask(): void {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.taskService.createTask(this.taskForm.value, token).subscribe(response => {
+        console.log('Tarea creada:', response);
+      }, error => {
+        console.error('Error al crear la tarea:', error);
+      });
+    } else {
+      console.error('No se encontró un token');
+    }
+  }
   
 }
---
+++ src/app/services/create-tasks.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CreateTasksService {
+
+  private apiUrl = 'http://localhost:8000/assign-task/';
+
+  constructor(private http: HttpClient) {}
+
+  // Método para crear una tarea
+  createTask(taskData: any, token: string): Observable<any> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.post(this.apiUrl, taskData, { headers });
+  }
+}
