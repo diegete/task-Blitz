e41a23295df644fc66eb001deee10e0fb03572b5
diegete <di.estay@duocuc.cl>
2024-10-06 17:12:32 -0300

    nuevo componente y router según tipo usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { UserViewComponent } from './components/user-view/user-view.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent},
     {path: 'home', component:HomeComponent},
-    {path: 'login', component:LoginComponent}
+    {path: 'login', component:LoginComponent},
+    {path: 'user-view', component:UserViewComponent},
     
 ];
