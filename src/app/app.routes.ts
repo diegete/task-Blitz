b170818944430599ad85f39378a0c2c8f6db7b5b
diegete <di.estay@duocuc.cl>
2024-11-25 21:05:18 -0300

    componente change-pass

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,15 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { UserViewComponent } from './components/user-view/user-view.component';
 import { PasswordResetComponent } from './components/password-reset/password-reset.component';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent},
     {path: 'home', component:HomeComponent},
     {path: 'login', component:LoginComponent},
     {path: 'user-view', component:UserViewComponent},
-    { path: 'auth/password-reset', component: PasswordResetComponent }
+    { path: 'auth/password-reset', component: PasswordResetComponent },
+    { path: 'auth/password-reset/:token', component: ChangePasswordComponent }
+
     
 ];
---
+++ src/app/components/change-password/change-password.component.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-change-password',
+  standalone: true,
+  imports: [CommonModule,FormsModule],
+  templateUrl: './change-password.component.html',
+  styleUrl: './change-password.component.css'
+})
+export class ChangePasswordComponent {
+  newPassword: string = '';
+  confirmPassword: string = '';
+  token: string = '';
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
+    this.token = this.route.snapshot.paramMap.get('token') || '';
+  }
+  onSubmit() {
+    if (this.newPassword !== this.confirmPassword) {
+      alert('Las contraseñas no coinciden.');
+      return;
+    }
+  
+    // Usa el token dinámico en la URL
+    const resetUrl = `http://localhost:8000/api/password/reset/${this.token}/`;
+    const body = { token: this.token, new_password: this.newPassword, confirm_password: this.confirmPassword };
+  
+    this.http.post(resetUrl, body).subscribe(
+      (response) => alert('Contraseña cambiada exitosamente.'),
+      (error) => alert('Error al cambiar la contraseña.')
+    );
+  }
+  
+}
